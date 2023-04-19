a4c368d6ae368fd43955bd1fd2ab2c2dbcc2b750
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-04-19 23:38:23 +0300

    Complete the method which accepts such an array, and returns that single different number.

---
+++ 7ky/stray.js
@@ -0,0 +1,9 @@
+function stray(numbers) {
+
+    for(var i in numbers){
+        if(numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])){
+            return numbers[i]
+        }
+    }
+
+}
