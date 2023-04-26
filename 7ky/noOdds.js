9e53d02dc9bd1c312529de106dcff9b5b0cdc654
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-04-26 23:38:28 +0300

    Write a small function that returns the values of an array that are not odd.

---
+++ 7ky/noOdds.js
@@ -0,0 +1,10 @@
+function noOdds(values) {
+
+    let result = []
+
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] % 2 === 0)
+            result.push(values[i])
+    }
+    return result
+}
