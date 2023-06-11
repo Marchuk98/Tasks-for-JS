16b9ae87a52147f3691535ceff499ce179a721d8
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-06-11 23:42:04 +0300

    build a calculator.

---
+++ 7ky/Calculator.js
@@ -0,0 +1,17 @@
+var Calculator = {
+    add: function (a, b) {
+        return a + b;
+    },
+    subtract: function (a, b) {
+        return a - b;
+    },
+    multiply: function (a, b) {
+        return a * b;
+    },
+    divide: function (a, b) {
+        if (b === 0) {
+            return false; // Division by zero is not allowed
+        }
+        return a / b;
+    }
+};
