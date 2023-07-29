bc71cb4d144b1c38e9f1c0ff46ccc59e5a6fdc41
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-07-29 23:25:17 +0300

    Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.

---
+++ 8ky/greet.js
@@ -0,0 +1,30 @@
+function greet(language) {
+
+    const greetings = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso',
+    };
+
+    const lowercaseLanguage = language.toLowerCase();
+
+    if (greetings.hasOwnProperty(lowercaseLanguage)) {
+        return greetings[lowercaseLanguage];
+    }
+
+    return greetings.english;
+}
