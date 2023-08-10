9f326cb563f7d3cf73e4643a8d9dfcf50d9f11fb
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-08-10 23:58:58 +0300

    Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.

---
+++ 6ky/isPangram.js
@@ -0,0 +1,12 @@
+function isPangram(string){
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const lowerCaseString = string.toLowerCase();
+
+    for (let char of alphabet) {
+        if (!lowerCaseString.includes(char)) {
+            return false;
+        }
+    }
+
+    return true;
+}
