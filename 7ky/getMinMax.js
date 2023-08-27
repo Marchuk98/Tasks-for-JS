c9d290c6e21516d07c0c89d6ab4c9f7c576d7dcf
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-08-27 23:48:22 +0300

    Implement a function that returns the minimal and the maximal value of a list (in this order).

---
+++ 7ky/getMinMax.js
@@ -0,0 +1,19 @@
+function getMinMax(arr){
+    if (arr.length === 0) {
+        return [];
+    }
+
+    let minValue = arr[0];
+    let maxValue = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < minValue) {
+            minValue = arr[i];
+        }
+        if (arr[i] > maxValue) {
+            maxValue = arr[i];
+        }
+    }
+
+    return [minValue, maxValue];
+};
