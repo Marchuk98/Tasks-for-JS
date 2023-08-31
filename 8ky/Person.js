aa22fe19ada0714902cccc95c88f690c6be92019
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-08-31 16:00:00 +0300

    complete the erson object, by completing the illriends function to fill the _friends rray for the person object.

---
+++ 8ky/Person.js
@@ -0,0 +1,10 @@
+var Person = function(){
+    var person = {
+        _name: "Leroy",
+        _friends: [],
+        fillFriends(f) {
+            f.forEach(friend => this._friends.push(friend));
+        }
+    };
+    return person;
+}
