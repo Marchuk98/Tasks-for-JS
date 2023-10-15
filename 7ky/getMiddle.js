e0186fa5db9e16960e4a5147f266c31e233223a8
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-10-15 23:56:56 +0300

    You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.

---
+++ 7ky/getMiddle.js
@@ -0,0 +1,10 @@
+function getMiddle(s)
+{
+    let length = s.length;
+    if (length % 2 !== 0) {
+        return s[Math.floor(length / 2)];
+    }
+    else {
+        return s[length / 2 - 1] + s[length / 2];
+    }
+}
