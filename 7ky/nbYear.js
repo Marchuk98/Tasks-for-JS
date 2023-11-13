35bedb5fdb89617ddb0c9b22c5e4cf30e2329395
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-11-13 23:58:20 +0300

    In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?

---
+++ 7ky/nbYear.js
@@ -0,0 +1,10 @@
+function nbYear(p0, percent, aug, p) {
+    percent = percent / 100;
+    let years = 0;
+    while (p0 < p) {
+
+        p0 = p0 + p0 * percent + aug;
+        years++;
+    }
+    return years;
+}
